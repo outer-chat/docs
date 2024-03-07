406f1b9f2a2f49ae1688d479c0b688ced532a6d2
Jean-Yanis Jeffroy <jean-yanis.jeffroy@epitech.eu>
2024-03-07 18:03:40 +0100

    feat(deployement): github pages

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,12 +8,12 @@ import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'Outer Chat Documentation',
+  tagline: 'Chat in outer space',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://docs.outer.chat',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
